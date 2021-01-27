d778f86a2a96cd4d3b266c44825ed7ac762b87e9
Oybek Beknazarov <oybekbeknazarov0896@gmail.com>
2021-01-27 23:10:39 +0500

    asfaslfk;las

---
+++ ChromiumTest/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ChromiumTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(99, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(66, 65);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(79, 39);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(116, 105);
+            this.panel2.TabIndex = 2;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+    }
+}
+
---
+++ ChromiumTest/Form1.cs
@@ -0,0 +1,157 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Windows.Forms;
+using CefSharp;
+using System.Threading.Tasks;
+using System.Dynamic;
+using System.Threading;
+using System.IO;
+
+namespace ChromiumTest
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public string signed;
+        private FsDLL fsDLL = new FsDLL();
+        private static WebServer webservers;
+        public static ChromiumWebBrowser browser;
+        Thread thrd;
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                if (File.Exists(Environment.CurrentDirectory + "\\debug.log"))
+                {
+                    File.Delete(Environment.CurrentDirectory + "\\debug.log");
+                }
+                CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+                Cef.Initialize(new CefSettings());
+                browser = new ChromiumWebBrowser("file:///" + Environment.CurrentDirectory + "//factura-send.html");
+                 
+                browser.JavascriptObjectRepository.NameConverter = null;
+
+                browser.JavascriptObjectRepository.Register("FsDLL", fsDLL, isAsync: false, options: new BindingOptions());
+                fsDLL.DataReady += WebServer.FsDLL_DataReady;
+                browser.IsBrowserInitializedChanged += Browser_IsBrowserInitializedChanged;
+
+                browser.RenderProcessMessageHandler = new RenderProcessMessageHandler();
+
+                panel1.Controls.Add(browser);
+                thrd = new Thread(GetCommand2);
+                thrd.Start();
+
+                contextMenuStrip = new ContextMenuStrip();
+                contextMenuStrip.Items.Add("Restart");
+                contextMenuStrip.Items.Add("Exit");
+                contextMenuStrip.ItemClicked += this.menuStrip1_ItemClicked;
+                contextMenuStrip.AutoClose = true;
+
+                bool MousePointerOnTaskBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+                if (this.WindowState == FormWindowState.Minimized && MousePointerOnTaskBar)
+                {
+                    notifyIcon1.Icon = this.Icon;
+                    this.ShowInTaskbar = false;
+                    notifyIcon1.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                thrd.Abort();
+                MessageBox.Show("Form1 " + ex.Message);
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            try
+            {
+
+                var item_text = e.ClickedItem.Text;
+
+                string respond = string.Empty;
+                this.ShowInTaskbar = false;
+        
+                if (e.ClickedItem.Text == "Restart")
+                {
+                    MessageBox.Show("Restart");
+                }
+                else if (e.ClickedItem.Text == "Exit")
+                {
+                    thrd.Abort();
+                    Application.Exit();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("menuStrip1_ItemClicked " + ex.Message);
+            }
+        }
+        public async void GetCommand2(object arg)
+        {
+            try
+            {
+                string[] prefixes = new string[] { "http://*:8808/" };
+                webservers = new WebServer(prefixes);
+                //WriteToFile("webservers " + prefixes[0]);
+                webservers.run(prefixes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("GetCommand2 " + ex.Message);
+            }
+        }
+
+        private void FsDLL_DataReady(object sender, string e)
+        {
+            MessageBox.Show("Data: " + e);
+            Console.WriteLine(e);
+        }
+
+        private void Browser_IsBrowserInitializedChanged(object sender, EventArgs e)
+        {
+            //browser.ShowDevTools();
+        }
+
+        public class RenderProcessMessageHandler : IRenderProcessMessageHandler
+        {
+            public void OnContextReleased(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame)
+            {
+                const string script = "document.addEventListener('OnContextReleased', function(){ alert('OnContextReleased'); });";
+
+                frame.ExecuteJavaScriptAsync(script);
+            }
+
+            public void OnFocusedNodeChanged(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IDomNode node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void OnUncaughtException(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, JavascriptException exception)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Wait for the underlying JavaScript Context to be created. This is only called for the main frame.
+            // If the page has no JavaScript, no context will be created.
+            void IRenderProcessMessageHandler.OnContextCreated(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+            {
+              //  const string script = "document.addEventListener('DOMContentLoaded', function(){ alert('DomLoaded'); });";
+
+               // frame.ExecuteJavaScriptAsync(script);
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip.Show(MousePosition);
+            }
+        }
+    }
+}
